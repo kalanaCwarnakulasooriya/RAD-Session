19dc970574c43803ee3cc0b8776746aea1c69095
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-09 20:10:44 +0530

    index.js

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+let name = "Kalana"
+let age = 18
+
+// console.log("Name : " + name) // bad practice
+console.log(`Name : ${name}`) // back tik atule doller mark ekth ekka js use karanna puluwn
+console.log(`Age : ${age}`)
+console.log(object);
